266304537fe205e4e53e7e8cb88e80d6b0f9623b
Quan (Uri) K.LE <uri@enclave.vn>
2020-12-24 16:55:36 +0700

    Update Format Code

--- lib/routs/routs.dart
+++ lib/routs/routs.dart
@@ -1,10 +1,12 @@
-import 'package:e_commerce/screens/forgot_password_screen/forgot_password.dart';
-import 'package:e_commerce/screens/sign_in_screen/sign_in.dart';
+import 'package:e_commerce/screens/forgot_password_screen/forgot_password_screen.dart';
+import 'package:e_commerce/screens/sign_in_screen/sign_in_screen.dart';
 import 'package:e_commerce/screens/splash_screen/splash_creen.dart';
+import 'package:e_commerce/screens/success_screen/success_screen.dart';
 import 'package:flutter/widgets.dart';
 
 final Map<String, WidgetBuilder> routes = {
   SplashScreen.routName: (context) => SplashScreen(),
   SignInScreen.routName: (context) => SignInScreen(),
   ForgotPasswordScreen.routName: (context) => ForgotPasswordScreen(),
+  SuccessScreen.routName: (context) => SuccessScreen()
 };
--- lib/screens/success_screen/success_screen.dart
+++ lib/screens/success_screen/success_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class SuccessScreen extends StatelessWidget {
+  static String routName = "/success_screen";
+
   @override
   Widget build(BuildContext context) {
     return Container();
