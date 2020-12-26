498ca3cbfb8cd3d1da345a1b34266a5fae89014b
lkquan2303 <55189723+lkquan2303@users.noreply.github.com>
2020-12-26 17:53:21 +0700

    Fix Bug: Sign Up Screen

--- lib/screens/sign_up_screen/details_sign_up/components/body_details_sign_up.dart
+++ lib/screens/sign_up_screen/details_sign_up/components/body_details_sign_up.dart
@@ -1,21 +1,27 @@
 import 'package:e_commerce/components/tittle_style.dart';
+import 'package:e_commerce/config/size_config.dart';
 import 'package:flutter/material.dart';
-
 import 'details_sign_up_form.dart';
 
 class BodyDetailsSignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: Column(
-        children: [
-          Text(
-            "Complete Profile",
-            style: tittleStyle(),
+    return Padding(
+      padding:
+          EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+      child: SingleChildScrollView(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            children: [
+              Text(
+                "Complete Profile",
+                style: tittleStyle(),
+              ),
+              DetailsSignUpForm(),
+            ],
           ),
-          DetailsSignUpForm()
-        ],
+        ),
       ),
     );
   }
--- lib/screens/sign_up_screen/details_sign_up/components/details_sign_up_form.dart
+++ lib/screens/sign_up_screen/details_sign_up/components/details_sign_up_form.dart
@@ -1,3 +1,5 @@
+import 'package:e_commerce/components/default_button.dart';
+import 'package:e_commerce/components/form_error.dart';
 import 'package:e_commerce/config/constants.dart';
 import 'package:e_commerce/config/size_config.dart';
 import 'package:flutter/material.dart';
@@ -8,44 +10,114 @@ class DetailsSignUpForm extends StatefulWidget {
 }
 
 class _DetailsSignUpFormState extends State<DetailsSignUpForm> {
+  final _formKey = GlobalKey<FormState>();
+  String email;
+  String password;
+  String rePassword;
+  List<String> errors = [];
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding:
-          EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+    return Form(
+      key: _formKey,
       child: Column(
         children: [
           SizedBox(
             height: getProportionateScreenHeight(80),
           ),
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: "Email",
-              hintText: "Enter your email",
-              labelStyle: TextStyle(color: kTextColor),
-            ),
-          ),
+          buildEmailSignUpFormField(),
           SizedBox(
             height: getProportionateScreenHeight(20),
           ),
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: "Password",
-              hintText: "Enter your password",
-              labelStyle: TextStyle(color: kTextColor),
-            ),
-          ),
+          buildPasswordSignUpFormField(),
           SizedBox(
             height: getProportionateScreenHeight(20),
           ),
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: "Re-Password",
-              hintText: "Enter your password again",
-              labelStyle: TextStyle(color: kTextColor),
-            ),
+          buildRePasswordSignUpFormField(),
+          SizedBox(
+            height: getProportionateScreenHeight(5),
+          ),
+          FormError(errors: errors),
+          SizedBox(
+            height: getProportionateScreenHeight(60),
+          ),
+          DefaultButton(
+            buttonText: "Continue",
+            buttonPress: () {
+              if (_formKey.currentState.validate()) {
+                _formKey.currentState.save();
+              }
+            },
           ),
         ],
+      ),
+    );
+  }
+
+  TextFormField buildRePasswordSignUpFormField() {
+    return TextFormField(
+      validator: (value) {
+        if (value.isEmpty && !errors.contains(kPassNullError)) {
+          setState(() {
+            errors.add(kPassNullError);
+          });
+        }
+        return "";
+      },
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: "Re-Password",
+        hintText: "Enter your password again",
+        labelStyle: TextStyle(color: kTextColor),
+      ),
+    );
+  }
+
+  TextFormField buildPasswordSignUpFormField() {
+    return TextFormField(
+      validator: (value) {
+        if (value.isEmpty && !errors.contains(kPassNullError)) {
+          setState(() {
+            errors.add(kPassNullError);
+          });
+        }
+        return "";
+      },
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: "Password",
+        hintText: "Enter your password",
+        labelStyle: TextStyle(color: kTextColor),
+      ),
+    );
+  }
+
+  TextFormField buildEmailSignUpFormField() {
+    return TextFormField(
+      onChanged: (value) {
+        if (errors.contains(kEmailNullError)) {
+          setState(() {
+            errors.remove(kEmailNullError);
+          });
+        }
+      },
+      validator: (value) {
+        if (value.isEmpty && !errors.contains(kEmailNullError)) {
+          setState(() {
+            errors.add(kEmailNullError);
+          });
+        } else if (!emailValidatorRegExp.hasMatch(value) &&
+            !errors.contains(kInvalidEmailError)) {
+          setState(() {
+            errors.add(kInvalidEmailError);
+          });
+        }
+        return "";
+      },
+      decoration: InputDecoration(
+        labelText: "Email",
+        hintText: "Enter your email",
+        labelStyle: TextStyle(color: kTextColor),
       ),
     );
   }
